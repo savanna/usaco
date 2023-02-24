6a32ab454410265fb2e731c8b466bcf0c66bb023
Xiaohui Chen <savanna@gmail.com>
2023-02-23 19:28:12 -0800

    2020 F S 1, timeout.

--- test.cpp
+++ test.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <iterator>
 #include <map>
+#include <ostream>
 #include <set>
 #include <sstream>
 #include <string>
@@ -17,138 +18,101 @@
 #include <utility>
 #include <vector>
 #include <cmath>
+#include <optional>
 
+using namespace std;
+
+
+int main() {
 //#define TESTING
 
 #ifdef TESTING
-const char *test_input = R"(6
-E 3 5
-N 5 3
-E 4 6
-E 10 4
-N 11 1
-E 9 2
+  const char *test_input = R"(7 4 7
+1 2
+2 3
+3 4
+4 5
 )";
 
-std::stringstream io(test_input);
+  std::stringstream in(test_input);
+  std::ostream& out = std::cout;
+
+#else  //!TESTING
+
+#define USING_FILE
+
+#ifdef USING_FILE
+  std::string input_file = "swap.in";
+  std::string output_file = "swap.out";
+  std::ofstream out;
+  out.open(output_file);
+  std::ifstream in;
+  in.open(input_file);
 
 #else
-std::istream& io = std::cin;
+  std::istream& in = std::cin;
+  std::ostream& out = std::cout;
+
+#endif
 #endif
 
+  constexpr int kLimit = 100'999;
 
-struct P {
-  int x;
-  int y;
-  char d;
-  int id;
-  int life = -1;
-  int blocked_by = -1;
-};
+  // cows and their positions.
+  std::array<int, kLimit> cows;
 
-int main() {
-  constexpr std::size_t kLimit = 1099;
+  std::vector<std::pair<int, int>> moves;
 
-  int N;
-  io >> N;
+  int N, M, K;
+  in >> N >> M >> K;
 
-  std::array<P, kLimit> points;
-
-  // second int in the pair is the index in `points` which serves as the id.
-  std::map<int, std::pair<int, int>> east_y_x;
-  std::map<int, std::pair<int, int>> north_x_y;
-
-  std::array<int, kLimit> block_count;
-
-  for (int i = 0; i < N; i++) {
-    io >> points[i].d >> points[i].x >> points[i].y;
-    points[i].id = i;
-
-    if (points[i].d == 'E') {
-      east_y_x[points[i].y] = {points[i].x, i};
-    } else {
-      assert(points[i].d == 'N');
-      north_x_y[points[i].x] = {points[i].y, i};
-    }
-  }
-
-  std::function<void(P&)> get_blocker = [&](P& p){
-    if (p.d == 'E') {
-      auto iter = north_x_y.lower_bound(p.x);
-
-      while(iter != north_x_y.end()) {
-        P& p2 = points[iter->second.second];
-
-        int time_to_hit = p2.x - p.x;
-        int time_to_setup = p.y - p2.y;
-        if (time_to_setup > 0 && time_to_setup < time_to_hit) {
-          // potentially, let's check the blocker's life
-          if (p2.life < 0) {
-            get_blocker(p2);
-          }
-
-          if (time_to_setup < p2.life) {
-            // indeed blocked.
-            p.blocked_by = p2.id;
-            p.life = time_to_hit;
-            break;
-          }
-        }
-        iter++;
-      }
-
-      if (p.life < 0) {
-        p.life = INT32_MAX;
-      }
-    } else { // 'N'
-      auto iter = east_y_x.lower_bound(p.y);
-
-      while(iter != east_y_x.end()) {
-        P& p2 = points[iter->second.second];
-
-        int time_to_hit = p2.y - p.y;
-        int time_to_setup = p.x - p2.x;
-        if (time_to_setup > 0 && time_to_setup < time_to_hit) {
-          // potentially, let's check the blocker's life
-          if (p2.life < 0) {
-            get_blocker(p2);
-          }
-
-          if (time_to_setup < p2.life) {
-            // indeed blocked.
-            p.blocked_by = p2.id;
-            p.life = time_to_hit;
-            break;
-          }
-        }
-        iter++;
-      }
-      if (p.life < 0) {
-        p.life = INT32_MAX;
-      }
-    }
-  };
-
-  for (int i = 0; i < N; i++) {
-    if (points[i].life < 0) {
-      get_blocker(points[i]);
-    }
+  for (int i = 0; i < M; i++) {
+    int x, y;
+    in >> x >> y;
+    moves.push_back({x -1, y -1});
   }
 
   for (int i = 0; i < N; i++) {
-    int current = i;
-    while(current >= 0) {
-      if (points[current].blocked_by >=0) {
-        block_count[points[current].blocked_by]++;
+    cows[i] = i;
+  }
+
+  // get the initial transformation.
+  for (int i = 0; i < M; i++) {
+    std::reverse(cows.begin() + moves[i].first, cows.begin() + moves[i].second + 1);
+  }
+
+  // positive is repeat#, negative is parent.
+  std::array<std::optional<int>, kLimit> parent;
+  // for each cow, try loop through the positions we just got, and stop as soon as it repeats.
+  for (int n = 0; n < N; n++) {
+    int repeat = 0;
+    std::optional<int>& p = parent[n];
+    while (p.has_value()) {
+      if(p < 0) {
+        // parent
+        p = parent[-1 * p.value()];
+      } else {
+        repeat = p.value();
+        break;
       }
-      current = points[current].blocked_by;
     }
-  }
 
-  for (int i = 0; i < N; i++) {
-    std::cout << block_count[i] << std::endl;
-  }
+    int current_position = n;  // n is starting position
+    int i = repeat ? K - K%repeat : 0;
+    for (; i < K; i++) {
+      current_position = cows[current_position];
+      repeat++;
 
-  return 0;
+      if (current_position == n) {
+        // it repeats, now we can stop repeating.
+        i = K - K%repeat - 1;
+        parent[n] = repeat;
+      } else {
+        parent[current_position] = -1 * n;
+      }
+    }
+
+    out << current_position + 1 << std::endl;
+  }
 }
 
